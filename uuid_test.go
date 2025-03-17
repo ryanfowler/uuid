1f026d93afa3ce3f6aa57da0d23f5f2ac9ceb881
Ryan Fowler <ryan.fowler19@gmail.com>
2025-03-16 17:39:00 -0700

    Support Go versions 1.23 and 1.24

--- uuid_test.go
+++ uuid_test.go
@@ -2,10 +2,13 @@ package uuid
 
 import (
 	"bytes"
+	"crypto/rand"
 	"database/sql"
 	"database/sql/driver"
 	"encoding"
 	"encoding/json"
+	"io"
+	mrand "math/rand/v2"
 	"testing"
 	"time"
 )
@@ -493,5 +496,32 @@ func BenchmarkFormatString(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		_ = u.String()
+	}
+}
+
+func BenchmarkNewV4ChaCha8(b *testing.B) {
+	var seed [32]byte
+	_, err := io.ReadFull(rand.Reader, seed[:])
+	if err != nil {
+		b.Fatalf("unable to read from crypto/rand: %s", err.Error())
+	}
+	r := mrand.NewChaCha8(seed)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, _ = NewV4FromRand(r)
+	}
+}
+
+func BenchmarkNewV7ChaCha8(b *testing.B) {
+	var seed [32]byte
+	_, err := io.ReadFull(rand.Reader, seed[:])
+	if err != nil {
+		b.Fatalf("unable to read from crypto/rand: %s", err.Error())
+	}
+	now := time.Unix(1000, 0)
+	r := mrand.NewChaCha8(seed)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, _ = NewV7FromRand(now, r)
 	}
 }
